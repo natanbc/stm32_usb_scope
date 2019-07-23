b59861cad80fbc1262b792d7578cf0ff01da8108
natanbc <natanbc@users.noreply.github.com>
2019-07-23 12:58:54 -0300

    Add support for remaining ADC pins

--- source/adc_config.h
+++ source/adc_config.h
@@ -9,27 +9,52 @@
 each branch needs to define:
 
 INPUT_PIN -> line
+INPUT_SMPR2 -> int
 
-INPUT_SMPR1, INPUT_SMPR2,
-INPUT_SQR1, INPUT_SQR2
--> ints
+SMPR 1 is only needed for channels > 9, which aren't available
+in this board.
 
-INPUT_SQR3 has the same definition regardless of input
+SQR 1, 2 and 3 have the same definition regardless of input
 because it only changes depending on the number of channels.
 Since we only support a single channel, there's only one possible
 definition of it.
 */
 
-//TODO: other inputs
-
-#if INPUT_CHANNEL == ADC_CHANNEL_IN8
+#if INPUT_CHANNEL == ADC_CHANNEL_IN9
+    #define INPUT_PIN           PAL_LINE(GPIOB, 1)
+    #define INPUT_SMPR2         ADC_SMPR2_SMP_AN9(INPUT_SAMPLE_RATE)
+#elif INPUT_CHANNEL == ADC_CHANNEL_IN8
     #define INPUT_PIN           PAL_LINE(GPIOB, 0)
-    #define INPUT_SMPR1         0
     #define INPUT_SMPR2         ADC_SMPR2_SMP_AN8(INPUT_SAMPLE_RATE)
-    #define INPUT_SQR1          0
-    #define INPUT_SQR2          0
+#elif INPUT_CHANNEL == ADC_CHANNEL_IN7
+    #define INPUT_PIN           PAL_LINE(GPIOA, 7)
+    #define INPUT_SMPR2         ADC_SMPR2_SMP_AN7(INPUT_SAMPLE_RATE)
+#elif INPUT_CHANNEL == ADC_CHANNEL_IN6
+    #define INPUT_PIN           PAL_LINE(GPIOA, 6)
+    #define INPUT_SMPR2         ADC_SMPR2_SMP_AN6(INPUT_SAMPLE_RATE)
+#elif INPUT_CHANNEL == ADC_CHANNEL_IN5
+    #define INPUT_PIN           PAL_LINE(GPIOA, 5)
+    #define INPUT_SMPR2         ADC_SMPR2_SMP_AN5(INPUT_SAMPLE_RATE)
+#elif INPUT_CHANNEL == ADC_CHANNEL_IN4
+    #define INPUT_PIN           PAL_LINE(GPIOA, 4)
+    #define INPUT_SMPR2         ADC_SMPR2_SMP_AN4(INPUT_SAMPLE_RATE)
+#elif INPUT_CHANNEL == ADC_CHANNEL_IN3
+    #define INPUT_PIN           PAL_LINE(GPIOA, 3)
+    #define INPUT_SMPR2         ADC_SMPR2_SMP_AN3(INPUT_SAMPLE_RATE)
+#elif INPUT_CHANNEL == ADC_CHANNEL_IN2
+    #define INPUT_PIN           PAL_LINE(GPIOA, 2)
+    #define INPUT_SMPR2         ADC_SMPR2_SMP_AN2(INPUT_SAMPLE_RATE)
+#elif INPUT_CHANNEL == ADC_CHANNEL_IN1
+    #define INPUT_PIN           PAL_LINE(GPIOA, 1)
+    #define INPUT_SMPR2         ADC_SMPR2_SMP_AN1(INPUT_SAMPLE_RATE)
+#elif INPUT_CHANNEL == ADC_CHANNEL_IN0
+    #define INPUT_PIN           PAL_LINE(GPIOA, 0)
+    #define INPUT_SMPR2         ADC_SMPR2_SMP_AN0(INPUT_SAMPLE_RATE)
 #else
     #error "Unsupported ADC channel " #INPUT_CHANNEL
 #endif
 
+#define INPUT_SMPR1             0
+#define INPUT_SQR1              0
+#define INPUT_SQR2              0
 #define INPUT_SQR3              ADC_SQR3_SQ1_N(INPUT_CHANNEL)
